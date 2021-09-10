c3608ea2dcac3d700873270aadd0ede200107aaf
GitHub Action <action@github.com>
2021-09-10 14:58:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/1160135186/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/1160135186/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.1160135186.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.1160135186.plugin.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/1160135186/plugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/1160135186/plugin/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.1160135186.plugin
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
